534b3dd7276a45c1d09e1ea93c0cff7989fff9f0
srp3110 <surrya20@gmail.com>
2022-10-26 02:28:21 +0800

    part 1 and 2 working

--- Coding/JavaScript/book-order.js
+++ Coding/JavaScript/book-order.js
@@ -1,68 +1,27 @@
-function calculate(unt_prc, qty, ttl){
-    let x = document.getElementById(unt_prc).value;
-    x = parseFloat(x).toFixed(2);
-    document.getElementById(unt_prc).value = x;
-    let y = document.getElementById(qty).value;
-    y = parseFloat(y).toFixed(0);
-    document.getElementById(qty).value = y;
-    let xy = x*y;
-    xy = parseFloat(xy).toFixed(2);
-    document.getElementById(ttl).value = xy;
-}
-function first_row(){
-    let unt_prc = "unit_price_1";
-    let qty = "quantity_1";
-    let ttl = "total_1";
-    calculate(unt_prc, qty, ttl);
-}
-function second_row(){
-    let unt_prc = "unit_price_2";
-    let qty = "quantity_2";
-    let ttl = "total_2";
-    calculate(unt_prc, qty, ttl);
-}
-function third_row(){
-    let unt_prc = "unit_price_3";
-    let qty = "quantity_3";
-    let ttl = "total_3";
-    calculate(unt_prc, qty, ttl);
-}
-function fourth_row(){
-    let unt_prc = "unit_price_4";
-    let qty = "quantity_4";
-    let ttl = "total_4";
-    calculate(unt_prc, qty, ttl);
-}
-function fifth_row(){
-    let unt_prc = "unit_price_5";
-    let qty = "quantity_5";
-    let ttl = "total_5";
-    calculate(unt_prc, qty, ttl);
-}
-
-function grandTotal(){
-    let ttl_1 = document.getElementById("total_1").value;
-    ttl_1 = parseFloat(ttl_1);
-    let ttl_2 = document.getElementById("total_2").value;
-    ttl_2 = parseFloat(ttl_2);
-    let ttl_3 = document.getElementById("total_3").value;
-    ttl_3 = parseFloat(ttl_3);
-    let ttl_4 = document.getElementById("total_4").value;
-    ttl_4 = parseFloat(ttl_4);
-    let ttl_5 = document.getElementById("total_5").value;
-    ttl_5 = parseFloat(ttl_5);
-    let g_ttl = ttl_1 + ttl_2 + ttl_3 + ttl_4 + ttl_5;
-    g_ttl = parseFloat(g_ttl).toFixed(2);
-    document.getElementById("sumGrandTotal").value = g_ttl;
-}
-
 function calculateTotal(){
-    first_row()
-    second_row()
-    third_row()
-    fourth_row()
-    fifth_row()
-    grandTotal()
+    let counter_1 = 1, counter_2 = 1, price = 0, quantity = 0, total = 0, calcGrandTotal = 0, negativeValue = false;
+    while (counter_1 <= 5) {
+        if (document.getElementById(`price_${counter_1}`).value < 0) {
+            document.getElementById(`price_${counter_1}`).value = 0;
+            negativeValue = true;
+        } if (document.getElementById(`quantity_${counter_1}`).value < 0) {
+            document.getElementById(`quantity_${counter_1}`).value = 0;
+            negativeValue = true;
+        }
+        counter_1++
+    }
+    if (negativeValue == true) {
+        alert("Invalid value detected")
+    }
+    while (counter_2 <= 5) {
+        price = parseFloat(document.getElementById(`price_${counter_2}`).value);
+        price = price.toFixed(2)
+        quantity = parseFloat(document.getElementById(`quantity_${counter_2}`).value);
+        quantity = quantity.toFixed(0)
+        total = price * quantity;
+        document.getElementById(`total_${counter_2}`).value = total.toFixed(2);
+        calcGrandTotal += total;
+        document.getElementById("sumGrandTotal").value = calcGrandTotal;
+        counter_2++;
+    }
 }
-
-// document.getElementById("calculateGrandTotal").onclick = calculateTotal();
