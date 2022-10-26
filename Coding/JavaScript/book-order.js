8b277c23dc98c9df020aa4d2737a6a757be338d8
srp3110 <surrya20@gmail.com>
2022-10-26 18:17:30 +0800

    finished

--- Coding/JavaScript/book-order.js
+++ Coding/JavaScript/book-order.js
@@ -2,26 +2,25 @@ function calculateTotal(){
     let counter_1 = 1, counter_2 = 1, price = 0, quantity = 0, total = 0, calcGrandTotal = 0, negativeValue = false;
     while (counter_1 <= 5) {
         if (document.getElementById(`price_${counter_1}`).value < 0) {
-            document.getElementById(`price_${counter_1}`).value = 0;
             negativeValue = true;
+            let x = prompt("ERROR: Unit Price value cannot be negative. Enter a valid value for the unit price.")
+            document.getElementById(`price_${counter_1}`).value = x;
         } if (document.getElementById(`quantity_${counter_1}`).value < 0) {
-            document.getElementById(`quantity_${counter_1}`).value = 0;
             negativeValue = true;
+            let y = prompt("ERROR: Quantity value cannot be negative. Enter a valid value for the quantity.")
+            document.getElementById(`quantity_${counter_1}`).value = y;
         }
         counter_1++
-    }
-    if (negativeValue == true) {
-        alert("Invalid value detected");
     }
     while (counter_2 <= 5) {
         price = parseFloat(document.getElementById(`price_${counter_2}`).value);
         price = price.toFixed(2);
         quantity = parseFloat(document.getElementById(`quantity_${counter_2}`).value);
-        quantity = quantity.toFixed(0);
+        quantity = Math.round(quantity);
         total = price * quantity;
         document.getElementById(`total_${counter_2}`).value = total.toFixed(2);
         calcGrandTotal += total;
-        document.getElementById("sumGrandTotal").value = calcGrandTotal;
+        document.getElementById("sumGrandTotal").value = calcGrandTotal.toFixed(2);
         counter_2++
     }
 }
